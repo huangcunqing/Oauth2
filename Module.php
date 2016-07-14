834693922b1991503aa7671c1ab1cc47873ba8fe
cunqinghuang <cunqinghuang@xmisp.com>
2016-07-14 11:33:28 +0800

    11

--- Module.php
+++ Module.php
@@ -10,36 +10,25 @@ class Module extends \yii\base\Module{
 
 
 
-   public function request($authorize_url,$client_id,$redirect_uri,$scope="user"){
-            $req = new oauth2;
-            $req-> request1($authorize_url,$client_id,$redirect_uri,$scope);
-
-   }
-   public function authorize($client_id,$client_secret,$redirect_uri,$uri,$uri_token){
-            $req = new oauth2;
-            $req->authorize1($client_id,$client_secret,$redirect_uri,$uri,$uri_token);
-    }
-   public function back1($homepage){
-        echo "<script> window.location.href =\"$homepage\"; </script>";
-   }
-//   public function re($email,$password,$username,$name="user"){
-//            $res = new resit;
-//           return $res->reg($email,$password,$username,$name);
+//   public function request($authorize_url,$client_id,$redirect_uri,$scope="user"){
+//            $req = new oauth2;
+//           return $req-> request1($authorize_url,$client_id,$redirect_uri,$scope);
+//
+//   }
+//   public function authorize($client_id,$client_secret,$redirect_uri,$uri,$uri_token){
+//            $req = new oauth2;
+//           return $req->authorize1($client_id,$client_secret,$redirect_uri,$uri,$uri_token);
 //    }
-
-//   public function entry($username,$password){
-//
-//       $data = array("username"=>$username,"password"=>$password);
-//       $data_string = json_encode($data);
-//       $ch = curl_init('http://backend.userauth.local/login');
-//       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-//       curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-//       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//       curl_setopt($ch, CURLOPT_HTTPHEADER,'Content-Type: application/json');
-//
-//
-//       $result = curl_exec($ch);
-//       curl_close($ch);
+//   public function back1($homepage){
+//           $req = new oauth2;
+//          return $req-> back1($homepage);
 //   }
 
+   public function authorize($authorize_url,$client_id,$redirect_uri,$scope="user",$client_secret,$uri,$uri_token,$homepage){
+       $req = new oauth2;
+       $req-> request1($authorize_url,$client_id,$redirect_uri,$scope);
+       $req->authorize1($client_id,$client_secret,$redirect_uri,$uri,$uri_token);
+       $req-> back1($homepage);
+
+   }
 }
